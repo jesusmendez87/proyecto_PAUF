b530e416d42fe5de33ac153d01aee6b4db177f77
jesusmendez87 <alerta.acuario@gmail.com>
2025-12-19 23:02:08 +0100

    render5

--- server.js
+++ server.js
@@ -218,12 +218,10 @@ app.delete("/api/delete/:type/:id", async (req, res) =>  {
 
 });
 
-const PORT = process.env.PORT || 10000;
+const port = process.env.PORT || 4000;
 
-
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-} 
-
+app.listen(port, () => {
+  console.log(` app listening on port ${port}`)
+}
 
 );
