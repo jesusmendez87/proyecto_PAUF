42a65715b2166af5dd2f8aeeb6b69e923ed28c2e
jesusmendez87 <72827621+jesusmendez87@users.noreply.github.com>
2025-12-21 09:52:09 +0100

    Import path and URL utilities in server.js
    Added path and URL utilities for file handling.
    
    Signed-off-by: jesusmendez87 <72827621+jesusmendez87@users.noreply.github.com>

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ import dotenv from 'dotenv';
 import Usuario from './src/app/core/models/user.js';
 import Partido from './src/app/core/models/partidos.js';
 import Equipo from './src/app/core/models/equipos.js';
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
@@ -12,6 +17,14 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+/* API */
+app.use("/api", require("./routes")); 
+
+app.use(express.static(angularDistPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(angularDistPath, "index.html"));
+});
 
 // Conexión a MongoDB
 mongoose
