da47fc046a8569dd058af24aa749341e2900f20b
jesusmendez87 <72827621+jesusmendez87@users.noreply.github.com>
2025-12-21 10:25:18 +0100

    Update Angular catch-all route handler
    Signed-off-by: jesusmendez87 <72827621+jesusmendez87@users.noreply.github.com>

--- server.js
+++ server.js
@@ -126,7 +126,7 @@ app.delete("/api/delete/:type/:id", async (req, res) => {
 });
 
 // Angular catch-all
-app.get("*", (req, res) => {
+app.get('/:all(*)', handler) => {
   res.sendFile(path.join(angularDistPath, "index.html"));
 });
 
