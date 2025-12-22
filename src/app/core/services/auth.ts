95f3506285f2012a3ab4775bfa39a20255960f52
jesusmendez87 <72827621+jesusmendez87@users.noreply.github.com>
2025-12-22 19:14:55 +0100

    Change apiUrl to relative path for AuthService
    Signed-off-by: jesusmendez87 <72827621+jesusmendez87@users.noreply.github.com>

--- src/app/core/services/auth.ts
+++ src/app/core/services/auth.ts
@@ -11,7 +11,7 @@ export interface User {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/api/login';
+  private apiUrl = '/api/login';
 
  private currentUserSubject = new BehaviorSubject<User | null>(
   (() => {
